109d4e0f4f7875bbc2b72f2d0dd45ca3fba7fb88
Mikhail <Semikolenov>
2017-01-27 22:20:03 +0600

    Simple rectangular selection (32x32 rectangle) on captured image.

--- WebCamTarget/Scripts/app/app.ts
+++ WebCamTarget/Scripts/app/app.ts
@@ -1,11 +1,21 @@
 declare var Webcam: any;
 
 
+class ScreenPoint {
+    public x: number;
+    public y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class WebCamTargetApp {
 
     private FRAME_RATE: number = 200;
 
     private canvas: HTMLCanvasElement;
+    private selectionCanvas: HTMLCanvasElement;
 
     private newFrame: ImageData;
     private oldFrame: ImageData;
@@ -14,8 +24,17 @@ class WebCamTargetApp {
 
         Rx.Observable.fromEvent(document, "DOMContentLoaded")
             .subscribe(() => {
+
+                this.selectionCanvas = document.querySelector("#selection") as HTMLCanvasElement;
+
                 this.initApp()
                     .subscribe(() => {
+
+                        Rx.Observable.fromEvent(this.selectionCanvas, "click")
+                            .subscribe((e) => {
+                                this.newSelection(e);
+                            });
+
                         Rx.Observable
                             .interval(this.FRAME_RATE)
                             .subscribe(() => {
@@ -61,7 +80,22 @@ class WebCamTargetApp {
         });
     }
 
+    private newSelection(e: MouseEvent): void
+    {
 
+        var pos: ScreenPoint = this.getMousePos(this.selectionCanvas, e);
+
+        var ctx: CanvasRenderingContext2D = this.selectionCanvas.getContext("2d");
+
+        ctx.beginPath();
+        ctx.lineWidth = 6;
+        ctx.strokeStyle = "green";
+        ctx.rect(pos.x - 16, pos.y-16, 32, 32);
+        ctx.stroke();
+        console.log((pos.x - 16).toString() + " : " + (pos.y - 16).toString());
+
+
+    }
 
     private costFunction(oldImage: Uint8ClampedArray, newImage: Uint8ClampedArray): number
     {
@@ -78,6 +112,14 @@ class WebCamTargetApp {
         }
 
         return Math.round(result / oldImage.length);
+    }
+
+    private getMousePos(canvas: HTMLCanvasElement, evt: MouseEvent): ScreenPoint
+    {
+        var rect = canvas.getBoundingClientRect();
+        return new ScreenPoint(evt.clientX - rect.left,
+            evt.clientY - rect.top);
+        
     }
 
 }
